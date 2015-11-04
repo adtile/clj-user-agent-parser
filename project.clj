880b3f67d72d882b0616bcd23c556851894877f3
Markus Hjort <markus.hjort@gmail.com>
2015-11-04 12:46:51 +0200

    Initial implementation

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject clj-user-agent-parser "0.0.1"
+  :description "Library for parsing user agent header"
+  :url "https://github.com/adtile/clj-user-agent-parser"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [cz.mallat.uasparser/uasparser "0.6.2"]])
---
+++ src/user_agent_parser/parser.clj
@@ -0,0 +1,32 @@
+(ns user-agent-parser.parser
+  (:require [clojure.string :as string]
+            [clojure.java.io :refer [input-stream resource]])
+  (:import [cz.mallat.uasparser UASparser MultithreadedUASparser]))
+
+(defn- string->keyword [^String string-key]
+  (-> (string/join "-" (-> string-key
+                           (.replaceAll "([A-Z]+)([A-Z][a-z])" "$1-$2")
+                           (.replaceAll "([a-z\\d])([A-Z])" "$1-$2")
+                           (string/split  #" ")))
+      (string/lower-case)
+      (keyword)))
+
+(defn create [^String path]
+  (MultithreadedUASparser. (-> path resource input-stream)))
+
+(defn parse [^UASparser parser ^String user-agent-string]
+  (let [user-agent (. parser parse user-agent-string)
+        has-info (. user-agent hasDeviceInfo)
+        is-robot (. user-agent isRobot)
+        browser-family (. user-agent getUaFamily)
+        browser-name (. user-agent getUaName)
+        device-type (. user-agent getType)
+        os-family (. user-agent getOsFamily)
+        os-name (. user-agent getOsName)]
+    {:has-info? has-info
+     :robot? is-robot
+     :browser-family (string->keyword browser-family)
+     :browser-name (string->keyword browser-name)
+     :device-type (string->keyword device-type)
+     :os-family (string->keyword os-family)
+     :os-name (string->keyword os-name)}))
