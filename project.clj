da1851e91c2d30d66cb9c7ce6a622741cddd9295
Markus Hjort <markus.hjort@gmail.com>
2015-11-04 12:54:11 +0200

    Add license info

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject clj-user-agent-parser "0.0.1"
   :description "Library for parsing user agent header"
   :url "https://github.com/adtile/clj-user-agent-parser"
+  :license {:name "MIT"}
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [cz.mallat.uasparser/uasparser "0.6.2"]])
