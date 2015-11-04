3793f6ef0449334835df72f5860c0fb9c7ff513e
Markus Hjort <markus.hjort@gmail.com>
2015-11-04 12:54:22 +0200

    Add gpg signing settings

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "Library for parsing user agent header"
   :url "https://github.com/adtile/clj-user-agent-parser"
   :license {:name "MIT"}
+  :signing {:gpg-key "webmaster@adtile.me"}
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [cz.mallat.uasparser/uasparser "0.6.2"]])
